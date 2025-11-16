20445c67a0addaa9fc150368d80606ad9002079c
Alexander Pogorelov <ics.price@gmail.com>
2025-11-16 14:09:42 +0300

    hw19: added homework19

---
+++ src/main/java/ru/otus/java/basic/homeworks/homework19/Fruit.java
@@ -0,0 +1,13 @@
+package ru.otus.java.basic.homeworks.homework19;
+
+public class Fruit {
+    protected double itemWeight;
+
+    public Fruit(double itemWeight) {
+        this.itemWeight = itemWeight;
+    }
+
+    public double getItemWeight() {
+        return itemWeight;
+    }
+}
