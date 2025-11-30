18fc8da6f19eaff692d9f363db041e158ac165ef
Alexander Pogorelov <ics.price@gmail.com>
2025-11-30 13:05:48 +0300

    hw23-homework: added homework 23

--- http-server/src/main/java/ru/otus/java/basic/http/server/Application.java
+++ http-server/src/main/java/ru/otus/java/basic/http/server/Application.java
@@ -2,7 +2,8 @@ package ru.otus.java.basic.http.server;
 
 public class Application {
     public static final int PORT = 8189;
+    public static final int THREADS_NUMBER = 4;
     public static void main(String[] args) {
-        new HttpServer(PORT).start();
+        new HttpServer(PORT).start(THREADS_NUMBER);
     }
 }
--- http-server/src/main/java/ru/otus/java/basic/http/server/HttpServer.java
+++ http-server/src/main/java/ru/otus/java/basic/http/server/HttpServer.java
@@ -3,6 +3,8 @@ package ru.otus.java.basic.http.server;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class HttpServer {
     private final int port;
@@ -13,21 +15,45 @@ public class HttpServer {
         this.dispatcher = new Dispatcher();
     }
 
-    public void start() {
+    public void start(int threadNumber) {
+        ExecutorService executor = Executors.newFixedThreadPool(threadNumber);
+
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             System.out.println("Сервер запущен на порту: " + port);
-            try (Socket socket = serverSocket.accept()) {
-                byte[] buffer = new byte[8192];
-                int n = socket.getInputStream().read(buffer);
-                if (n < 1) {
-                    return;
+            while (true) {
+                try {
+                    Socket socket = serverSocket.accept();
+                    executor.execute(() -> {
+                        try {
+                            byte[] buffer = new byte[8192];
+                            int n = socket.getInputStream().read(buffer);
+                            System.out.println("Запрос начал обрабатывать поток " + Thread.currentThread().getName());
+                            if (n < 1) {
+                                return;
+                            }
+                            String rawRequest = new String(buffer, 0, n);
+                            HttpRequest request = new HttpRequest(rawRequest);
+                            dispatcher.execute(request, socket.getOutputStream());
+                            System.out.println("Запрос обработал поток " + Thread.currentThread().getName());
+                        } catch (IOException e) {
+                            System.err.println("Ошибка при обработке запроса: " + e.getMessage());
+                        } finally {
+                            try {
+                                socket.close();
+                            } catch (IOException e) {
+                                //
+                            }
+                        }
+                    });
+                } catch (IOException e) {
+                    System.err.println("Ошибка при принятии соединения: " + e.getMessage());
+                    break;
                 }
-                String rawRequest = new String(buffer, 0, n);
-                HttpRequest request = new HttpRequest(rawRequest);
-                dispatcher.execute(request, socket.getOutputStream());
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            System.err.println("Не удалось запустить сервер на порту " + port + ": " + e.getMessage());
+        } finally {
+            executor.shutdown();
         }
     }
 }
